30631c2c38bf9a6d78fc79a6df01f64624232959
danichax2022-stack <danichax2022@gmail.com>
2026-01-30 14:37:09 -0500

    ok

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,31 @@
+
 import type { Metadata } from "next";
 import { GameProvider } from "../context/GameContext";
 import { TonConnectProvider } from '@/components/TonConnectProvider';
 import { SocketProvider } from '../context/SocketContext';
 import "./globals.css";
 
-// ...
+export const metadata: Metadata = {
+    title: "Cartel Wars",
+    description: "Plata o Plomo",
+};
 
-<body>
-    <TonConnectProvider>
-        <SocketProvider>
-            <GameProvider>
-                {children}
-            </GameProvider>
-        </SocketProvider>
-    </TonConnectProvider>
-</body>
-        </html >
+export default function RootLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <html lang="en">
+            <body>
+                <TonConnectProvider>
+                    <SocketProvider>
+                        <GameProvider>
+                            {children}
+                        </GameProvider>
+                    </SocketProvider>
+                </TonConnectProvider>
+            </body>
+        </html>
     );
 }
