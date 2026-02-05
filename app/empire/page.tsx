ce5ed425eda125e3c3f9f15dfeb88950dd161fb6
danichax2022-stack <danichax2022@gmail.com>
2026-02-05 10:31:07 -0500

    Create page.tsx

---
+++ app/empire/page.tsx
@@ -0,0 +1,10 @@
+import MainLayout from '../../components/MainLayout';
+import Empire from '../../views/Empire';
+
+export default function EmpirePage() {
+    return (
+        <MainLayout>
+            <Empire />
+        </MainLayout>
+    );
+}
